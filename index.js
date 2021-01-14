ecfa46ad890a12f0376def0e3bf8963852a9bda2
burakhalefoglu <42536490+burakhalefoglu@users.noreply.github.com>
2021-01-14 20:09:05 +0300

    last
    last

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 
-
 const PORT = process.env.PORT || 3000;
 const INDEX = '/index.html';
-
 const express= require('express');
+
+
 const server = express()
   .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
@@ -12,6 +12,7 @@ const server = express()
 
 const wss = new Server({ server });
 
+//This function will work whenever client connects to the server or disconnects.
 wss.on('connection', (ws) => {
   console.log('Client connected');
   ws.on('close', () => console.log('Client disconnected'));
